6489592c484dea4ef7e27657c628e7846d9ac5eb
davetii <davetii@gmail.com>
2018-12-30 22:41:06 -0500

    Reworked the errors returned

--- src/main/java/com/greatwideweb/kanbantool/web/ProjectController.java
+++ src/main/java/com/greatwideweb/kanbantool/web/ProjectController.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -25,7 +27,11 @@ public class ProjectController {
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project p, BindingResult result) {
         if (result.hasErrors()) {
-            return new ResponseEntity<List<FieldError>>(result.getFieldErrors(), HttpStatus.BAD_REQUEST);
+            Map<String, String> errorMap = new HashMap();
+            for (FieldError e : result.getFieldErrors()) {
+                errorMap.put(e.getField(), e.getDefaultMessage());
+            }
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
         }
         Project project = projectService.saveForUpdateProject(p);
         return new ResponseEntity(project, HttpStatus.CREATED);
