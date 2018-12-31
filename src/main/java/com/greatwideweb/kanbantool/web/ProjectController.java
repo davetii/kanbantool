0fd136541e2295e19a849724ec531ddbfed11237
davetii <davetii@gmail.com>
2018-12-30 22:37:14 -0500

    Basic Result.getFieldErrors example

--- src/main/java/com/greatwideweb/kanbantool/web/ProjectController.java
+++ src/main/java/com/greatwideweb/kanbantool/web/ProjectController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/project")
@@ -23,7 +25,7 @@ public class ProjectController {
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project p, BindingResult result) {
         if (result.hasErrors()) {
-            return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<List<FieldError>>(result.getFieldErrors(), HttpStatus.BAD_REQUEST);
         }
         Project project = projectService.saveForUpdateProject(p);
         return new ResponseEntity(project, HttpStatus.CREATED);
